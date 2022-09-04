101b10c4c9cb46813252455b7143a859810d3ee3
Banadikt <banadikt10@gmail.com>
2022-09-04 01:55:33 +0100

    first test layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SidebarComponent} from "./app-layout/sidebar/sidebar.component";
+import {AppLayoutComponent} from "./app-layout/app-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppLayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './app-layout/sidebar/sidebar.component';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { MainContentComponent } from './app-layout/main-content/main-content.component';
+import { HeaderComponent } from './app-layout/header/header.component';
+import { FooterComponent } from './app-layout/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    AppLayoutComponent,
+    MainContentComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
